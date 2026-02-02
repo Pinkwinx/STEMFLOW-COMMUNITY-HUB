109f9ba7defeef2101cbe327f2738e6c08bec3d6
Maria Nguyen <47044508+Pinkwinx@users.noreply.github.com>
2026-02-02 09:14:09 -0800

    Deploy changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename="/STEMFLOW-COMMUNITY-HUB/"></BrowserRouter>
     <App />
   </StrictMode>,
 )
